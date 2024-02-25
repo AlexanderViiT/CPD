1cc931f19a6ce255127d02c8369d8267546be849
AlexanderViiT <saquigold@gmail.com>
2024-02-25 01:18:40 -0600

    Creación de Proyecto
    Se crean las clases:
    CPD\App.xaml
    CPD\App.xaml.cs
    CPD\AssemblyInfo.cs
    CPD\CPD.csproj
    CPD\CPD.sln
    CPD\MainWindow.xaml
    CPD\MainWindow.xaml.cs
    
    Sin camibios, sin ediciones, directo de Visual Studio

---
+++ CPD/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CPD
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
